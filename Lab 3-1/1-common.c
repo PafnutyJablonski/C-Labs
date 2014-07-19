7b33a54942bdd9a098a9631031beff144eb17d98
Ilya Bulochkin <Frislin@users.noreply.github.com>
2014-07-19 19:05:28 +1100

    Исправить функцию сложения

--- Lab 3-1/1-common.c
+++ Lab 3-1/1-common.c
@@ -21,7 +21,7 @@ t_pfrac new_pfrac(int a, int b, int c){
         f.b = abs(b);
         f.c = abs(c) * s1;
     } else {
-        if(a ==0){
+        if(a == 0){
             f.a = 0;
             f.b = abs(b);
             f.c = abs(c) *s2;
@@ -36,8 +36,7 @@ t_pfrac new_pfrac(int a, int b, int c){
 }
 
 int correct_pfrac(t_pfrac f){
-    if(f.c == 0)
-         return 0;
+    if(f.c == 0) return 0;
     return 1;
 };
 
@@ -47,10 +46,8 @@ void print_pfrac(t_pfrac f){
 };
 
 t_pfrac pfrac_add(t_pfrac f1, t_pfrac f2){
-    if(f1.c == 0)
-        return f1;
-    if(f2.c == 0)
-        return f2;
+    if(f1.c == 0) return f1;
+    if(f2.c == 0) return f2;
     t_pfrac f;
     int nod1 = gcd(f1.b, f1.c);
     int nod2 = gcd(f2.b, f2.c);
@@ -76,44 +73,34 @@ t_pfrac pfrac_add(t_pfrac f1, t_pfrac f2){
     f.c = nok(f1.c, f2.c) * s2;
     f.b = (f.c / f1.c) * f1.b + (f.c / f2.c) * f2.b;
     f.a +=  f.b / abs(f.c);
-    f.b = f.b % f.c;
+    f.b = f.b % abs(f.c);
     return f;
 };
 
 t_pfrac pfrac_sub(t_pfrac f1, t_pfrac f2){
-    if(f1.c == 0)
-        return f1;
-    if(f2.c == 0)
-        return f2;
+    if(f1.c == 0) return f1;
+    if(f2.c == 0) return f2;
     t_pfrac f;
     f2.c *= (-1);
     return pfrac_add(f1, f2);
  };
 
 t_pfrac pfrac_mul(t_pfrac f1, t_pfrac f2){
-    if(f1.c == 0)
-        return f1;
-    if(f2.c == 0)
-        return f2;
+    if(f1.c == 0) return f1;
+    if(f2.c == 0) return f2;
     t_pfrac f;
-    //if(f1.b == f1.a == 0){  }
     f1.b = f1.b + f1.a * abs(f1.c);
     f2.b = f2.b + f2.a * abs(f2.c);
     f.b = f1.b * f2.b;
     f.c = f1.c * f2.c;
     f.a = abs(f.b) / abs(f.c);
     f.b = abs(f.b) % abs(f.c);
-    int nod = gcd(f.b, f.c);
-    f.b = f.b / nod;
-    f.c = f.c / nod;
     return f;
 };
 
 t_pfrac pfrac_div(t_pfrac f1, t_pfrac f2){
-    if(f1.c == 0)
-        return f1;
-    if(f2.c == 0)
-        return f2;
+    if(f1.c == 0) return f1;
+    if(f2.c == 0) return f2;
     int s2;
     t_pfrac f;
     (f2.c < 0)?(s2 = -1):(s2 = 1);
@@ -124,9 +111,16 @@ t_pfrac pfrac_div(t_pfrac f1, t_pfrac f2){
  };
 
 int cmp_pfrac(t_pfrac f1, t_pfrac f2){
-    int nok = f1.c/gcd(abs(f1.c), abs(f2.c))*abs(f2.c);
-    int f1b = f1.b * (nok/f1.c) + (f1.a * nok);
-    int f2b = f2.b * (nok/f2.c) + (f2.a * nok);
+    if(f1.c == 0) return 3;
+    if(f2.c == 0) return 3;
+    int s1, s2;
+    (f1.c < 0)?(s1 = -1):(s1 = 1);
+    (f2.c < 0)?(s2 = -1):(s2 = 1);
+    f1.c = f2.c = nok(f1.c, f2.c);
+    f1.b = f1.b + f1.a * abs(f1.c);
+    f2.b = f2.b + f2.a * abs(f2.c);
+    int f1b = s1 * (int)f1.b;
+    int f2b = s2 * (int)f2.b;
     if (f1b < f2b) {return -1;}
     if (f1b == f2b) {return 0;}
     if (f1b > f2b) {return 1;}
@@ -151,8 +145,8 @@ int nok(int a, int b){
 
 
 int main(void){
-    t_pfrac f1 = new_pfrac(0, 1, 3);
-    t_pfrac f2 = new_pfrac(0, -2, 3);
-    print_pfrac(pfrac_div(f1, f2));
+    t_pfrac f1 = new_pfrac(0, -2, 3);
+    t_pfrac f2 = new_pfrac(0, -5, 3);
+    print_pfrac(pfrac_add(f1, f2));
     return 0;
 };
